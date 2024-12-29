678f42f48aa3840517fecd1b9dcbe9f7fa698c3b
Madhur Verma <madhurverma@Madhurs-MacBook-Air.local>
2024-12-29 17:05:01 +0530

    Require practice

---
+++ app.js
@@ -0,0 +1,14 @@
+
+require("./xyz.js")  // one module into another
+
+var name = "Namaste NodeJs";
+
+var a = 20;
+var b = 30;
+
+console.log(name);
+
+console.log(a+b);
+
+
+// console.log(globalThis)
